f498cefe03d2c1849a8d1c18bcada1b3e9448018
Pam <pausuarez.270@gmail.com>
2026-01-11 18:58:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que se recargue la página
-    
-    // Aquí podrías validar el DNI, pero por ahora lo mandamos directo al dashboard
-    window.location.href = "dashboard.html"; 
+  window.location.href = "dashboard.html"; 
 });
+// Simulamos que el cliente tiene cierta cantidad de puntos
+const puntosCliente = 1250; 
+
+// Buscamos el lugar en la pantalla donde escribiremos el rango
+const textoRango = document.getElementById('rango-cliente');
+
+// Lógica de niveles
+if (puntosCliente >= 300000) {
+    textoRango.innerText = "Eau de Parfum";
+    textoRango.style.color = "#d4af37"; // Un color dorado elegante
+} 
+else if (puntosCliente >= 100000) {
+    textoRango.innerText = "Elixir";
+    textoRango.style.color = "#e91e63"; // Rosa fuerte
+} 
+else {
+    textoRango.innerText = "Eau de Toilette";
+    textoRango.style.color = "#333"; // Color normal
+}
